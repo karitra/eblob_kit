acb6dabd73e28093010765b274001f61e4ee2bf7
Kirill Smorodinnikov <shaitkir@gmail.com>
2017-10-13 11:23:56 +0300

    v0.0.24

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='eblob_kit',
-      version='0.0.23',
+      version='0.0.24',
       author='Kirill Smorodinnikov',
       author_email='shaitkir@gmail.com',
       py_modules=['eblob_kit'],
