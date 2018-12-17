9bf9d38c99df20f4ddcebdb120219796f20b7bb3
karitra <karev.alexandre@gmail.com>
2018-12-17 03:54:21 +0300

    test: unit tests
    Unit tests for eblob_kit subjects: BlobRepairer, DataFile, DiskControl
    and get_checksum_size.

---
+++ tests/common.py
@@ -0,0 +1,59 @@
+"""Tests common stuff."""
+import struct
+
+import eblob_kit
+
+
+def generate_key(start=0):
+    """Generate valid key for testing."""
+    return ''.join([chr((start + x) % 256) for x in xrange(eblob_kit.DiskControl.key_size)])
+
+
+def make_header(key='', data_size=0, disk_size=0, flags=eblob_kit.RecordFlags(0), position=0):
+    """Create DiskControl with default values.
+
+    Parameters equals to eblob_kit.DiskControl constructor.
+
+    With all defaults arguments construct an 'empty header', usually such a header should be considered 'non valid',
+    used as a stub for tesing purposes.
+
+    :rtype eblob_kit.DiskControl
+    """
+    return eblob_kit.DiskControl(key=key, data_size=data_size, disk_size=disk_size, flags=flags, position=position)
+
+
+def header_to_bytes(header):
+    """Construct bytes sequence DiskControl header representation.
+
+    :param eblob_kit.DiskControl header: disk control header to pack into created buffer.
+
+    :rtype: str
+    """
+    fmt = '<{}s4Q'.format(eblob_kit.DiskControl.key_size)
+    return struct.pack(fmt, header.key, header.flags.flags, header.data_size, header.disk_size, header.position)
+
+
+def make_blob_byte_squence(sizes=None, fill=0xf1):
+    """Create a data blob.
+
+    :param (List[int] | None) sizes: list of data chunks sizes
+    :param int fill: fill the buffer with provided value.
+
+    :rtype: bytearray
+    """
+    position = 0
+    if sizes is None:
+        sizes = []
+
+    total_sizes = sum(size + eblob_kit.DiskControl.size for size in sizes)
+    buff = bytearray([fill] * total_sizes)
+
+    for key_sequence, size in enumerate(sizes):
+        disk_size = eblob_kit.DiskControl.size + size
+        header = make_header(key=generate_key(key_sequence), disk_size=disk_size, position=position)
+
+        buff[position:position+eblob_kit.DiskControl.size] = header_to_bytes(header)
+
+        position += disk_size
+
+    return buff
---
+++ tests/test_data_file.py
@@ -0,0 +1,100 @@
+"""DataFile tests."""
+import pytest
+
+import sys
+
+# Used for file access emulation.
+import StringIO
+
+import common
+import eblob_kit
+
+
+TEST_OFFSETS_RANGE = 7
+
+
+if sys.version_info < (3, 0):
+    OPEN_TO_PATCH = '__builtin__.open'
+else:
+    OPEN_TO_PATCH = 'builtins.open'
+
+
+def _make_buffer_with_valid_diskcontrol(buffer_size, disk_control_offset, disk_control):
+    b = bytearray(buffer_size)
+    b[disk_control_offset: disk_control_offset + eblob_kit.DiskControl.size] = common.header_to_bytes(disk_control)
+
+    return b
+
+
+@pytest.fixture(params=[
+    (1, 0, 0, 0, 0),
+    (1, 0x01, 3, 4, 5),
+    (3, 0xFFFF, 2**32, 2 ** 32, 2 ** 32),
+])
+def disk_control(request):
+    """Generate some valid DiskControl instance.
+
+    TODO(karapuz): add parameters with some marginal values.
+    TODO(karapuz): use named tuple for test cases.
+
+    """
+    return eblob_kit.DiskControl(key=common.generate_key(request.param[0]),
+                                 flags=eblob_kit.RecordFlags(request.param[1]),
+                                 data_size=request.param[2],
+                                 disk_size=request.param[3],
+                                 position=request.param[4])
+
+
+@pytest.mark.parametrize('disk_control_offset', xrange(TEST_OFFSETS_RANGE))
+def test_read_disk_control_valid(mocker, disk_control, disk_control_offset):
+    """Test read_disk_control along with eblob_kit.DataFile.read method."""
+    dummy_buffer = _make_buffer_with_valid_diskcontrol(eblob_kit.DiskControl.size * 2,
+                                                       disk_control_offset,
+                                                       disk_control)
+
+    with mocker.patch(OPEN_TO_PATCH, return_value=StringIO.StringIO(dummy_buffer)),\
+            mocker.patch('eblob_kit.DataFile.__len__', return_value=len(dummy_buffer)):
+
+        data_file = eblob_kit.DataFile('some/path')
+        dummy_disk_control = data_file.read_disk_control(disk_control_offset)
+
+        assert dummy_disk_control == disk_control
+
+
+@pytest.mark.xfail(raises=EOFError)
+@pytest.mark.parametrize('disk_control_offset', xrange(TEST_OFFSETS_RANGE))
+def test_read_disk_control_with_exception(mocker, disk_control, disk_control_offset):
+    """Test read_disk_control exceptions."""
+    dummy_buffer = _make_buffer_with_valid_diskcontrol(eblob_kit.DiskControl.size - 1,
+                                                       disk_control_offset,
+                                                       disk_control)
+
+    with mocker.patch(OPEN_TO_PATCH, return_value=StringIO.StringIO(dummy_buffer)),\
+            mocker.patch('eblob_kit.DataFile.__len__', return_value=len(dummy_buffer)):
+        eblob_kit.DataFile('some/path').read_disk_control(disk_control_offset)
+
+
+@pytest.mark.parametrize('sizes', [
+    [1, 2, 3],
+    [1],
+    [],
+    [0],
+    [0, 1],
+    [0, 1, 1024, 512, 3]
+])
+def test_data_file_iter(mocker, sizes):
+    """Check iterator over eblob_kit.DataFile records."""
+    dummy_buffer = common.make_blob_byte_squence(sizes)
+
+    with mocker.patch(OPEN_TO_PATCH, return_value=StringIO.StringIO(dummy_buffer)),\
+            mocker.patch('eblob_kit.DataFile.__len__', return_value=len(dummy_buffer)):
+
+        data_file = eblob_kit.DataFile('some/path')
+        cnt = 0
+        total_size = 0
+        for disk_control in data_file:
+            total_size += disk_control.disk_size
+            cnt += 1
+
+        assert cnt == len(sizes)
+        assert len(dummy_buffer) == total_size
