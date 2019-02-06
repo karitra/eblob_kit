a628f2f6da87e73a60a99b9c9e9cb5a60e338660
karitra <karev.alexandre@gmail.com>
2019-02-06 14:32:40 +0300

    v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='eblob_kit',
-      version='0.1.0',
+      version='0.1.2',
       author='Kirill Smorodinnikov',
       author_email='shaitkir@gmail.com',
       py_modules=['eblob_kit'],
